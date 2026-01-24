15d159a1bcf720ff7a1a34c8cff37984e1859a0d
alxfed <alxfed@gmail.com>
2026-01-24 12:49:29 -0600

    correction

--- src/gracchus/__init__.py
+++ src/gracchus/__init__.py
@@ -5,3 +5,9 @@
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
+from .rest import response, models
+
+__all__ = [
+    'response',
+    'models'
+]
